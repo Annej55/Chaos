68f0617ef195729cb814a7349d53ef7802186714
Jenna R <80995334+Annej55@users.noreply.github.com>
2023-12-17 12:25:46 -0700

    commented standard map code better

--- standard_map.py
+++ standard_map.py
@@ -4,37 +4,60 @@ import matplotlib.pyplot as plt
 import matplotlib.colors as mcolors
 import random
 import time
-from cycler import cycler
-
 
 def driver():
-
+    '''
+    Function parameters to change:
+    (x0, y0) - initial condition for a specific trajectory
+    N - Number of iterations for each initial condition
+    k - parameter
+    num_points - the number of points in both x and y to plot as initial conditions to fill out the map
+    manifold_appx_dist - how far from the fixed point along the eigenvectors to begin iterating to approximate the manifolds
+    '''
     x0 = .2
     y0 = .3
-    N = 500
-    k = 0
-    num_points = 50
+    N = 100
+    k = (np.pi/2)**2
+    num_points = 25
     manifold_appx_dist = 1e-10
 
-    x_traj, y_traj = initial_condition_trajectory(x0, y0, k, N)
-    
-    #plot_trajectory(x_traj, y_traj, N,'.')
+    # plots a single trajectory in the standard map starting from (x0, y0)
+    # plot_trajectory(x0, y0, N, k, '.') 
  
-    plot_map(N, k, num_points, x0, y0, manifold_appx_dist, init_val = True, sep_on = True, point_type = ',', a = 0.5, b = 0.5, eigvecs_on = True, manifold_on = True)
+    # plots general picture of entire map, optionally plots separatrix, manifold approximation, and a particular intial value
+    # see function def for details
+    plot_map(N, k, num_points, x0, y0, manifold_appx_dist, init_val_on = True, sep_on = True, manifold_on = True, point_type = ',', a = 0.5, b = 0.5)
 
-def plot_map(N, k, num_points, x_init, y_init, manifold_appx_dist, init_val = False, sep_on = False, point_type = ',', a = 0.5, b = 0.5,  eigvecs_on = False, manifold_on = False):
+def plot_map(N, k, num_points, x_init, y_init, manifold_appx_dist, init_val_on = False, sep_on = False, manifold_on = False, point_type = ',', a = 0.5, b = 0.5):
+    '''
+    Plots num_points^2 intial conditions to get a general picture of the behavior of the standard map.
+    Also plots the following (though these can be turned on or off with the boolean inputs):
+    - plots a paticular trajectory with (x_init, y_init) initial condition
+    - plots the separatrix of the (0,0) resonance/fixed point
+    - approximation of the manifolds (the approximation is bad for large k but still interesting)
+    Input:
+    - N - number of iterations for each initial point
+    - k - parameter
+    - num_points - number of x and y values to use as initial conditions. Total of num_points^2 initial conditions fill out the map
+    - x_init, y_init - starting x and y value of specific initial condition
+    - manifold_appx_dist - how far from the fixed point along the eigenvectors to begin iterating to approximate the manifolds
+    - init_val_on, sep_on, manifold_on - booleans turn the initial value, separatrix, and manifold plots on or off, default is off
+    - point_type - point style to plot the main map with
+    - a - x value max of plot
+    - b - y value max of plot
+    Output: plot of standard map
+    '''
+
     x0 = np.linspace(-a, a, num_points)
     y0 = np.linspace(-b, b, num_points)
-    if eigvecs_on:
+    if manifold_on:
         eigvals, eigvecs = Df_eigs(k, -0.5)
         eigvecs[:, 0] = normalize(eigvecs[:, 0])
         eigvecs[:, 1] = normalize(eigvecs[:, 1])
-
     if sep_on:
         y_separatrix = (1/np.pi)*np.sqrt(k/2 + (k/2)*np.cos(2*np.pi*x0))
-        y_separatrix_2 = .5*np.sqrt(1 + (k/(np.pi))**2 + ((k/(np.pi))**2)*np.cos(4*np.pi*x0))
-
-    random.seed(2)
+        # y_separatrix_2 = .5*np.sqrt(1 + (k/(np.pi))**2 + ((k/(np.pi))**2)*np.cos(4*np.pi*x0))
+    random.seed(3) # for colors
     plt.figure()    
 
     colors = ['#CC3366','#FF0066','#CC6699','#CC00FF','#9933FF','#9900FF','#00FFCC','#00FF33','#33CC00','#00CC99','#33CC99','#33FFFF','#33CCCC','#3399CC','#0000CC','#0099FF','#6633FF','#66CC99','#33CC66','#66CC00','#33FF33','#FF3333','#CC0033','#0000FF','#00CCFF','#CC0000']
@@ -44,14 +67,14 @@ def plot_map(N, k, num_points, x_init, y_init, manifold_appx_dist, init_val = Fa
             plt.plot(x_traj,y_traj, point_type, color = random.choice(colors))
     c1 = 'blue'
     c2 = 'blue'
-    if eigvecs_on:
+    if manifold_on:
         origin = np.array([[-.5, -.5], [0, 0]])
         if eigvals[0] > 1:
             c1 = 'red'
         if eigvals[1] > 1:
             c2 = 'red'
-        # plt.quiver(*origin, eigvecs[:, 0], eigvecs[:, 1], color=[c1, c2], scale=12)
-    if init_val:
+        # plt.quiver(*origin, eigvecs[:, 0], eigvecs[:, 1], color=[c1, c2], scale=12) # plots eigenvectors
+    if init_val_on:
         x_traj, y_traj = initial_condition_trajectory(x_init, y_init, k, N)
         lbl = 'Trajectory from ('+str(x_init)+','+str(y_init)+')'
         plt.plot(x_traj,y_traj,'.', color = 'b', label = lbl)
@@ -60,14 +83,14 @@ def plot_map(N, k, num_points, x_init, y_init, manifold_appx_dist, init_val = Fa
     if sep_on:
         plt.plot(x0, y_separatrix, 'k,-', label='separatrix')
         plt.plot(x0, -y_separatrix, 'k,-')
-        '''plt.plot(x0, y_separatrix_2, 'k,-', label='secondary separatrix')
-        plt.plot(x0, -y_separatrix_2+1, 'k,-')'''
-    if manifold_on and eigvecs_on:
-        x_man, y_man = iter_manifold(eigvals, eigvecs, 0, 0.5, 0, manifold_appx_dist)
+        # plt.plot(x0, y_separatrix_2, 'k,-', label='secondary separatrix')
+        # plt.plot(x0, -y_separatrix_2+1, 'k,-')
+    if manifold_on:
+        x_man, y_man = iter_manifold(eigvecs, 0, 0.5, 0, manifold_appx_dist)
         x_traj, y_traj = initial_condition_trajectory(x_man, y_man, k, 5*N)
         lbl = 'Manifold approximation'
         plt.plot(x_traj,y_traj,'m.', label = lbl)
-        x_man, y_man = iter_manifold(eigvals, eigvecs, 1, 0.5, 0, manifold_appx_dist)
+        x_man, y_man = iter_manifold(eigvecs, 1, 0.5, 0, manifold_appx_dist)
         x_traj, y_traj = initial_condition_trajectory(x_man, y_man, k, 5*N)
         plt.plot(x_traj,y_traj,'m.')
     plt.xlim(-a, a)
@@ -76,32 +99,37 @@ def plot_map(N, k, num_points, x_init, y_init, manifold_appx_dist, init_val = Fa
     plt.legend(loc='lower right')
     plt.show()
 
-def plot_trajectory(x_traj, y_traj, N, point_type = '.'):
-
+def plot_trajectory(x0, y0, N, k, point_type = '.'):
+    '''
+    Plots a single trajectory in the standard map starting from (x0, y0)
+    Input:
+    - (x0, y0) - intial condition
+    - N - number of iterations for each initial point
+    - k - parameter
+    - point_type - type of point used for plot
+    Output: Plot of single trajectory
+    '''
+    x_traj, y_traj = initial_condition_trajectory(x0, y0, k, N)
     plt.figure()    
     plt.plot(x_traj,y_traj,'b'+point_type, label= 'trajectory')
-    plt.plot(x_traj[0],y_traj[0], 'ro', label = 'starting point')
+    lbl = 'start ('+str(x0)+','+str(y0)+')'
+    plt.plot(x_traj[0],y_traj[0], 'ro', label = lbl)
     plt.plot(x_traj[N-1],y_traj[N-1], 'co', label = 'ending point')
     
     plt.xlim(-0.5, 0.5)
     plt.ylim(-0.5, 0.5)
     plt.legend()
+    plt.title('Standard Map with '+str(N)+' Iterations, k='+str(k))
     plt.show()   
 
-def point_color(x,y,x_max,y_max):
-    # The original colour of a point is uniquely determined by an RGB value determined by
-    # the starting point where Red is given by P0, Blue is given by Q0, and
-    # Green is given by P0+Q0.
-    # print(str(x)+','+str(y))
-    return (abs(x/x_max), abs(y/y_max), 0)
-
 def mod1(x):
+    # mods the x value to make the map correctly periodic (built in mod does not work)
     x_mod1 = x - np.floor(x+.5) 
     return x_mod1
 
-# x is generally p, y is generally q for Hamiltonian
 def chirikov(x, y, k):
     # One iteration of Chirikov's standard map with parameter k
+    # note: x is generally p, y is generally q for Hamiltonian
     y_prime = y - (k/(2*np.pi))*np.sin(2*np.pi*x)
     x_prime = mod1(x + y_prime)
     
@@ -109,9 +137,10 @@ def chirikov(x, y, k):
 
 def initial_condition_trajectory(x0, y0, k, N):
     # find the trajectory of intial condition (x0, y0)
-    ''' input: x0 - intial x, y0 - initial y, k - parameter, N - number of iterations '''
-
-    # calculate the first N points (x and y values) of the trajectory starting at (x0, y0)
+    ''' 
+    Input: x0 - intial x, y0 - initial y, k - parameter, N - number of iterations 
+    Output: first N points (x and y values) of the trajectory starting at (x0, y0)
+    '''
     x_traj = np.zeros(N)
     y_traj = np.zeros(N)
     x_traj[0], y_traj[0] = x0, y0
@@ -120,62 +149,57 @@ def initial_condition_trajectory(x0, y0, k, N):
 
     return x_traj, y_traj
 
-#def plot_trajectory(x_traj, y_traj, N, m):
-    # Plots the trajectories of each intial condition
-    # Input: x_traj - m x N matrix with each column containing the x values for a single trajectory (i.e for a given intial condition)
-    # y_traj - m x N matrix with each column containing the y values for a single trajectory (i.e for a given intial condition)
-    # N - the number of points in each trajectory (i.e. # iterations of (x0, y0)). Also the number of columns of the matrices x_traj and y_traj
-    # m - number of different trajectories. Also the rows of the matrices
-
-def iter_manifold(eigvals, norm_eigvecs, vec_col, x0, y0, L = 1e-10):
+def iter_manifold(norm_eigvecs, vec_col, x0, y0, L = 1e-10):
     # go in direction of stable and unstable eigenvector from saddle point
+    '''
+    Input:
+    - norm_eigvecs - normalized eigenvectors (2x2 matrix with columns as eigenvectors) at the fixed point
+    - vec_col - which column (eigenvector) of norm_eigvector to use
+    - (x0, y0) - saddle point
+    - L - distance along eigenvector to iterate from (x0, y0)
+    Output:
+    - point distance L away from saddle point along eigenvector
+    '''
     v1 = norm_eigvecs[0,vec_col]
     v2 = norm_eigvecs[1,vec_col]
     x_prime = x0 + L*v1
     y_prime = y0 + L*v2
     return x_prime, y_prime
 
-
 def Df_eigs(k, x0):
-    # returns eigenvalues of Df, the jacobian of the standard map at an intial condition x0
     '''
-    kcos = k*np.cos(2*np.pi*x0)
-    Df = np.zeros((2,2))
-    Df[0,0] = 1-kcos
-    Df[0,1] = 1
-    Df[1,0] = -kcos
-    Df[1,1] = 1
-    tr = 2 - kcos
-    discrim = np.sqrt(tr**2 - 4)
-    eigvals = [(-tr - discrim)/2, (-tr + discrim)/2]
-    vec = la.solve(Df)
+    Finds eigenvalues and eigenvectors of Df, the jacobian of the standard map at an intial condition x0
+    Input:
+    - k - parameter
+    - x0 - x value of point to evaluate eigenpairs at (y-value has no effect in this case)
+    Output:
+    - eigenvalue vector, length 2
+    - eigenvectors, 2x2 matrix with the jth column being the eigenvector corresponding to the jth entry of the eigenvalue vector. 2x2 in this case
+    '''
 
-    return eigvals '''
     kcos = k*np.cos(2*np.pi*x0)
     Df = np.zeros((2,2))
     Df[0,0] = 1-kcos
     Df[0,1] = 1
     Df[1,0] = -kcos
     Df[1,1] = 1
-    tr = 2 - kcos
+    # tr = 2 - kcos # Trace: not used in this but can give some analytical information, so I left it in
     
+    '''
+    Eigenvalues calculated more analytically - probably would be better than built in eig but for 2x2 shouldn't really matter.
+    I would probably use these to then find the eigenvectors if I continued working on this code
     tr = 2 - kcos
     discrim = np.sqrt(tr**2 - 4)
     eigvals = [(tr - discrim)/2, (tr + discrim)/2]
-    
-    '''A1 = Df
-    A2 = Df
-    A1[0,0] = A1[0,0] - eigvals[0]
-    A1[1,1] = A1[1,1] - eigvals[0]
+    '''
 
-    A2[0,0] = A2[0,0] - eigvals[1]
-    A2[1,1] = A2[1,1] - eigvals[1]'''
-
-
-    # print(la.eig(Df))
     return la.eig(Df)
 
 def normalize(v):
+    '''
+    normalizes a vector
+    - should account for zero vector, could almost certainly be done better, but should be sufficient for this use at least
+    '''
     norm = la.norm(v)
     if norm < 1e-16: 
        return v
